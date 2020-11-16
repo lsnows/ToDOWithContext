3034d20b8582f07c25bc6f43dda61bf5e80073fe
Neves <gabrielnds00@gmail.com>
2020-11-16 18:15:34 -0300

    Context Added

---
+++ src/Navigator.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import IndexScreen from './screens/IndexScreen';
+
+import { BlogProvider } from './context/BlogContext';
+
+const Stack = createStackNavigator();
+
+const Navigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="IndexScreen" component={IndexScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default () => {
+    return (
+        <BlogProvider>
+            <Navigator />
+        </BlogProvider>
+    );
+};
